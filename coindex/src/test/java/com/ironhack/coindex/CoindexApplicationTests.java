93f4258e6ed69499faa406d50e8d19f94b2ff522
Alejandro <amnaredo@gmail.com>
2021-03-06 17:38:16 +0100

    add project starter files

---
+++ coindex/src/test/java/com/ironhack/coindex/CoindexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.coindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
