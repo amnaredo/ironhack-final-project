16aa6bb629ad127530694301489e621bcad5b313
Alejandro <amnaredo@gmail.com>
2021-03-07 17:57:21 +0100

    update feign clients endpoints

--- portfolio-service/src/main/java/com/ironhack/portfolioservice/client/PositionClient.java
+++ portfolio-service/src/main/java/com/ironhack/portfolioservice/client/PositionClient.java
@@ -2,9 +2,7 @@ package com.ironhack.portfolioservice.client;
 
 import com.ironhack.portfolioservice.dto.PositionDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,14 +10,14 @@ import java.util.List;
 public interface PositionClient {
 
     @GetMapping("/positions/{id}")
-    PositionDTO getPosition(Long id);
+    PositionDTO getPosition(@PathVariable Long id);
 
     @PostMapping("/portfolios/{idPortfolio}/positions")
-    PositionDTO addPosition(Long idPortfolio, PositionDTO positionDTO);
+    PositionDTO addPosition(@PathVariable Long idPortfolio, @RequestBody PositionDTO positionDTO);
 
     @DeleteMapping("/positions/{id}")
-    void deletePosition(Long id);
+    void deletePosition(@PathVariable Long id);
 
     @GetMapping("/portfolios/{idPortfolio}/positions")
-    List<PositionDTO> getPortfolioPositions(Long idPortfolio);
+    List<PositionDTO> getPortfolioPositions(@PathVariable Long idPortfolio);
 }
--- portfolio-service/src/main/java/com/ironhack/portfolioservice/client/PositionUpdateClient.java
+++ portfolio-service/src/main/java/com/ironhack/portfolioservice/client/PositionUpdateClient.java
@@ -2,8 +2,11 @@ package com.ironhack.portfolioservice.client;
 
 import com.ironhack.portfolioservice.dto.PositionUpdateDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -11,11 +14,11 @@ import java.util.List;
 public interface PositionUpdateClient {
 
     @GetMapping("/updates/{id}")
-    PositionUpdateDTO getPositionUpdate(Long id);
+    PositionUpdateDTO getPositionUpdate(@PathVariable Long id);
 
     @PostMapping("/positions/{id}/updates")
-    PositionUpdateDTO addPositionUpdate(Long idPosition, PositionUpdateDTO positionUpdateDTO);
+    PositionUpdateDTO addPositionUpdate(@PathVariable Long idPosition, @RequestBody PositionUpdateDTO positionUpdateDTO);
 
     @GetMapping("/positions/{id}/updates")
-    List<PositionUpdateDTO> getPositionUpdates(Long idPosition);
+    List<PositionUpdateDTO> getPositionUpdates(@PathVariable Long idPosition);
 }
