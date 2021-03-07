4e10699c89a8dec968ecd7199916df5b7aef0eda
Alejandro <amnaredo@gmail.com>
2021-03-07 11:19:10 +0100

    add position update microservice

---
+++ position-update-service/src/main/java/com/ironhack/positionupdateservice/controller/impl/PositionUpdateController.java
@@ -0,0 +1,37 @@
+package com.ironhack.positionupdateservice.controller.impl;
+
+import com.ironhack.positionupdateservice.controller.interfaces.IPositionUpdateController;
+import com.ironhack.positionupdateservice.dto.PositionUpdateDTO;
+import com.ironhack.positionupdateservice.service.interfaces.IPositionUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PositionUpdateController implements IPositionUpdateController {
+    @Autowired
+    private IPositionUpdateService service;
+
+    @GetMapping("/updates/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PositionUpdateDTO getPositionUpdate(Long id) {
+        return service.getPositionUpdate(id);
+    }
+
+    @PostMapping("/positions/{id}/updates")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PositionUpdateDTO addPositionUpdate(Long idPosition, PositionUpdateDTO positionUpdateDTO) {
+        return service.addPositionUpdate(idPosition, positionUpdateDTO);
+    }
+
+    @GetMapping("/positions/{id}/updates")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PositionUpdateDTO> getPositionUpdates(Long idPosition) {
+        return service.getPositionUpdates(idPosition);
+    }
+}
