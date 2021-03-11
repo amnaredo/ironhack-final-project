c99d759b5f8144b61c20fb5c69ff1adbd766b168
Alejandro <amnaredo@gmail.com>
2021-03-11 19:33:23 +0100

    add portfolio total value calculation

--- frontend/src/app/portfolio-item/portfolio-item.component.ts
+++ frontend/src/app/portfolio-item/portfolio-item.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { Portfolio } from '../models/portfolio';
 import { PositionUpdate } from '../models/position-update';
+import { CoinApiService } from '../services/coin-api.service';
 import { PortfolioService } from '../services/portfolio.service';
 
 @Component({
@@ -15,12 +16,30 @@ export class PortfolioItemComponent implements OnInit {
 
   @Output() deletePortfolioEvent = new EventEmitter();
 
+  totalValue: number = 0;
+  includeSymbols: string[] = [];
+
   constructor(
+    private coinApiService: CoinApiService,
     private portfolioService: PortfolioService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.updateCoinsData();
+  }
+
+  updateCoinsData(): void {
+    this.includeSymbols.splice(0);
+    this.totalValue = 0;
+    this.portfolio.positions.forEach(position => {
+      const coinId = position.coinId;
+      const amount = position.amount;
+      this.coinApiService.getCoinById(coinId).subscribe(coinResult => {
+        this.totalValue = this.totalValue + coinResult.market_data.current_price.eur * amount;
+        this.includeSymbols.push(coinResult.symbol.toUpperCase());
+      })
+    });
   }
 
   // public get portfolio(): Portfolio {
