ff762295c77679779c3a4bf61a806925d70ab66b
Alejandro <amnaredo@gmail.com>
2021-03-13 18:37:46 +0100

    add backend readme file

---
+++ backend/databases.sql
@@ -0,0 +1,13 @@
+-- Create DBs for microservices
+
+DROP SCHEMA IF EXISTS coindex_position_update;
+CREATE SCHEMA coindex_position_update;
+
+DROP SCHEMA IF EXISTS coindex_position;
+CREATE SCHEMA coindex_position;
+
+DROP SCHEMA IF EXISTS coindex_portfolio;
+CREATE SCHEMA coindex_portfolio;
+
+DROP SCHEMA IF EXISTS coindex_user_profile;
+CREATE SCHEMA coindex_user_profile;
